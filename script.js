a65e236a2771d76a28e3e547dfd4537f628f0021
shekhrajik <rajik2539@gmail.com>
2025-09-06 12:55:41 +0530

    Added new feature

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-let input = document.querySelector('input')
-let form = document.querySelector('form')
-let ul = document.querySelector('ul')
 
-const saveTodo = (e) =>{
-    e.preventDefault()
-    let li = document.createElement('li')
-    li.innerText = input.value
-    li.className = "list-group-item rounded-0"
-    let  delbtn = document.createElement('button')
-    delbtn.innerText = "Delete"
-    delbtn.className = 'btn btn-sm btn-danger rounded-0 float-end'
-    li.appendChild(delbtn)
-    ul.appendChild(li)
-    form.reset()
+let input = document.querySelector('input');
+let form = document.querySelector('form');
+let ul = document.querySelector('ul');
+
+const getRandomColor = () => {
+    const colors = ['#ffadad', '#ffd6a5', '#c6ffb6ff', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff'];
+    return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const saveTodo = (e) => {
+    e.preventDefault();
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    li.className = "list-group-item rounded-0";
+    
+
+    li.style.backgroundColor = getRandomColor(); 
+
+    let delbtn = document.createElement('button');
+    delbtn.innerText = "Delete";
+    delbtn.className = 'btn btn-sm btn-danger rounded-0 float-end';
+    
+    li.appendChild(delbtn);
+    ul.appendChild(li);
+    form.reset();
 }
-form.addEventListener("submit",saveTodo)
+
+form.addEventListener("submit", saveTodo);
 
 const deleteTodo = (e) => {
-    if(e.target.className.includes('btn-danger')){
-        ul.removeChild(e.target.parentElement)
+    if(e.target.className.includes('btn-danger')) {
+        ul.removeChild(e.target.parentElement);
     }
 }
-ul.addEventListener("click",deleteTodo)
+ul.addEventListener("click", deleteTodo);
